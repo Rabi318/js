a7f99c3c70716eb4676a4f15fd826fc3c3c91494
RABINARAYAN SAHOO <130839369+Rabi318@users.noreply.github.com>
2023-10-27 12:21:00 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,25 @@
+let name="Rabi";
+
+let newName = Number(name);
+// console.log(newName);        //Nan
+// console.log(typeof newName);  //number
+
+//console.log(`Hello ${name} good evening`);
+//console.log(name.length);
+
+let js="JavaScript";
+// console.log(js.charAt(3));
+// console.log(js.indexOf('S'));
+const newStr = js.substring(1,5);
+//console.log(newStr);
+
+const newStr1  = js.slice(1,4);
+//console.log(newStr1);
+
+const str = "         java script     ";
+// console.log(str);
+// console.log(str.trim());
+
+const url="https://google.com/github";
+//console.log(url.replace('github','javascript'));  //https://google.com/javascript
+
