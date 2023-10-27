eb64065a794071d21764da929285cd2c25c356c6
RABINARAYAN SAHOO <130839369+Rabi318@users.noreply.github.com>
2023-10-27 15:22:08 +0000

    math

--- 01_basics/number_and_math.js
+++ 01_basics/number_and_math.js
@@ -15,4 +15,19 @@ const zero = 1000000;
 // console.log(zero.toLocaleString());          //1,000,000
 // console.log(zero.toLocaleString('en-IN'));  //10,00,000
 
+
 //+++++++++++++ Maths  +++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-61));  //It convert negative numbers into positive
+// console.log(Math.round(45.7));  //46
+// console.log(Math.round(45.2));  //45
+// console.log(Math.ceil(45.2));      //It round up the number to upper number
+// console.log(Math.floor(45.7));     //45
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
