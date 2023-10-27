8092ab066da584223efaa893561666cc2ef621d2
RABINARAYAN SAHOO <130839369+Rabi318@users.noreply.github.com>
2023-10-27 03:06:28 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score="33abc";
 
-console.log(typeof score);
+//console.log(typeof score);
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33" => 33
@@ -12,12 +12,38 @@ console.log(valueInNumber);
 
 let isLoggedIn="rabi";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false;
 // "" => false; "rabi" => true
 
 let someNumber=22;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *****************Operations*************//
+
+
+let value=5;
+let negValue=-value;
+//console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //2^3
+
+let str1="rabi";
+let str2="narayan";
+let str3=str1 + str2;
+//console.log(str3);
+
+// console.log("3" + 2);      //32
+// console.log(3 + "2");      //32
+// console.log("3" + 2 + 2);  //322
+// console.log(3 + 2 + "2");  //52
+
+// console.log(+true); //1
+// console.log(+"");   //0
+
