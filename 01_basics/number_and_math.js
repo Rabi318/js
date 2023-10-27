e66da70066562a8406866fb5c194c0dde8d53ed9
RABINARAYAN SAHOO <130839369+Rabi318@users.noreply.github.com>
2023-10-27 12:38:33 +0000

    numbers

---
+++ 01_basics/number_and_math.js
@@ -0,0 +1,18 @@
+//+++++++++++++  Numbers ++++++++++
+
+const score = 500;
+const balance = new  Number(120);
+// console.log(score);    //500
+// console.log(balance);  //[Number: 120]
+
+// console.log(balance.toString().length);  //3
+// console.log(balance.toFixed(2));         //120.00
+
+const num = 155.85666;
+//console.log(num.toPrecision(3)); //156
+
+const zero = 1000000;
+// console.log(zero.toLocaleString());          //1,000,000
+// console.log(zero.toLocaleString('en-IN'));  //10,00,000
+
+//+++++++++++++ Maths  +++++++++++++++++
