2253ab014079b18fc28c0d16ab91200dd019e3e3
RABINARAYAN SAHOO <130839369+Rabi318@users.noreply.github.com>
2023-10-27 04:55:14 +0000

    non-primitive

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -5,6 +5,9 @@ let age=23;
 let isLoggedIn=true;
 let state;
 
+console.log(age);
+console.log(typeof age);
+
 //primitive datatypes
 //number => 2^53 range
 //bigInt
@@ -13,9 +16,23 @@ let state;
 //null => standalone value, empty value
 // undefined => not assigned any value to the variable
 //symbol
+const id=Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId); //false, return value is not same
 
-//non-primitive datatypes
-//objects
 
-console.log(age);
-console.log(typeof age);
+//non-primitive (Reference type)datatypes
+//objects, array, functions
+
+const name1=["rabi", "ram", "shyam"]; //array
+
+let student ={
+    name:"rabi",
+    age : 21,
+    branch: "MCA",
+}                 //Objects
+
+//function
+const myFun=function (){
+    console.log("Hello World");
+}
