f8d56aab7792bab619de8becfee23bc66caffed5
RABINARAYAN SAHOO <130839369+Rabi318@users.noreply.github.com>
2023-10-27 04:37:27 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(3 < 2);
+// console.log(3 == 2);
+// console.log(3 != 2);
+
+// console.log("2" > 1);  //true
+// console.log("02" > 1);  //true
+
+// console.log(null > 0);   //false
+// console.log(null == 0);  //false
+// console.log(null >= 0);  //true
+
+// console.log(undefined ==0);  //false
+// console.log(undefined > 0);  //false
+// console.log(undefined < 0);  //false
+
+
+//Strictly comparision, without conversion.Check the datatypes 
+console.log("2" === 2);     //false   
+console.log("2" === "2");  //true
+
