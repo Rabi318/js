1ba25f98772868fdb7eb41673d8c09e0832d8fc3
RABINARAYAN SAHOO <130839369+Rabi318@users.noreply.github.com>
2023-10-28 05:31:13 +0000

    date and time

---
+++ 01_basics/date.js
@@ -0,0 +1,26 @@
+let myDate = new Date();
+//console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());     
+// console.log(typeof myDate);              //object
+
+// let myNewDate = new Date(2023, 1, 24);
+// console.log(myNewDate.toDateString());  //fri Feb 24 2023
+
+// let myNewDate = new Date("2023-01-26");
+// console.log(myNewDate.toLocaleDateString());   // 1/26/2023
+
+let myNewDate = new Date("2023-11-14");
+// console.log(myNewDate.toDateString());         //Tue Nov 14 2023
+
+let timeStamp = Date.now();
+// console.log(timeStamp);
+// console.log(myNewDate.getTime());
+
+// console.log(Math.floor(Date.now() / 1000));
+
+let newDate  = new Date();
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());            //6
+// console.log(newDate.getFullYear());      //2023
+
