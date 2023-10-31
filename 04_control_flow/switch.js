1ca2768f4bf182968de385af142cd161f09c4759
RABINARAYAN SAHOO <130839369+Rabi318@users.noreply.github.com>
2023-10-31 04:17:33 +0000

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,48 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 10;
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 4:
+//         console.log("Apr");
+//         break;
+//     case 10:
+//         console.log("Oct");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+const month = "oct";
+switch ("oct") {
+    case "jan":
+        console.log("Jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "oct":
+        console.log("oct");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
