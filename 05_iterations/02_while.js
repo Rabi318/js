4ff6e57733547d3cc7dd4754413dfe7ff6abacef
RABINARAYAN SAHOO <130839369+Rabi318@users.noreply.github.com>
2023-11-01 15:50:19 +0000

    while loop

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,29 @@
+//syntax
+
+// while(condition){
+
+// }
+
+// let i = 0;
+// while(i <= 5){
+//     console.log(`value of i is ${i}`);
+//     i = i + 2;
+// }
+
+// const MyArr = ["Ironman", "Spiderman", "Thor"];
+// let arr = 0;
+// while(arr < MyArr.length){
+//     console.log(`${MyArr[arr]}`);
+//     arr++;
+// }
+
+
+//+++++++++ do while +++++++
+
+let score = 1;
+do{
+    console.log(`Score is ${score}`);
+    score ++ ;
+}while(score <= 5);
+
+
