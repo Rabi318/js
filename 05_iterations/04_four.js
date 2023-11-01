9fb9e1d2c605c37a0705f793f14431bceb35842a
RABINARAYAN SAHOO <130839369+Rabi318@users.noreply.github.com>
2023-11-01 17:32:52 +0000

    forin loop

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,15 @@
+const myObj = {
+    js: "javaScript",
+    cpp: "C++",
+    rb: "ruby",
+};
+
+// for(const key in myObj){
+//     console.log(`${key} shortcut is for ${myObj[key]}`);
+// }
+
+const programming = ['java', 'javascript', 'c', 'html'];
+for(const key in programming){
+    console.log(programming[key]);
+}
+
